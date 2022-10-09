1f1986de64436b4a4147ee09312199a71432194e
Julien DUMONT <julien.dumont@wildsys.io>
2022-10-09 09:28:04 +0200

    feat: custom hook, cert creation and perform post-actions

--- renew.sh
+++ renew.sh
@@ -3,18 +3,23 @@
 # Required arguments
 DOMAINS=''
 POST_ACTIONS=''
+CONTACT_MAIL=''
 DRY_RUN='--dry-run'
-while getopts "d:p:E" opt
+ALT_HOOKS="$(dirname $0)/hooks/dns"
+while getopts "k:d:p:e:E" opt
 do
     case $opt
     in
         'd') DOMAINS=$OPTARG ;;
         'E') DRY_RUN='' ;;
         'p') POST_ACTIONS=$OPTARG ;;
+        'e') CONTACT_MAIL=$OPTARG ;;
+        'k') ALT_HOOKS=$OPTARG ;;
         ?) exit 1 ;;
     esac
 done
-test -z $DOMAINS && echo -e "Usage: $0 -d <domain(s)> [-E] [-p <post_actions>]" && exit 1
+test -z $DOMAINS && echo -e "Usage: $0 -d <domain(s)> -e <contact_email> [-E] [-p <post_actions>] [-k <hook_directory>]" && exit 1
+test -z $CONTACT_MAIL && echo -e "Usage: $0 -d <domain(s)> -e <contact_email> [-E] [-p <post_actions>] [-k <hook_directory>]" && exit 1
 
 # Certbot execution
 certbot \
@@ -23,10 +28,11 @@ certbot \
     --manual \
     --expand \
     $DRY_RUN \
+    --agree-tos \
+    --email $CONTACT_MAIL \
     --manual-public-ip-logging-ok \
     --preferred-challenges dns \
-    --manual-auth-hook $(dirname $0)/hooks/dns/auth.py \
-    --manual-cleanup-hook $(dirname $0)/hooks/dns/clean.py \
+    --manual-auth-hook ${ALT_HOOKS}/auth.py \
+    --manual-cleanup-hook ${ALT_HOOKS}/clean.py \
     certonly
-$POST_ACTIONS
-
+$($POST_ACTIONS)
